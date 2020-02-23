0cfcb39b31d8ce3dc17cacbfe01c037e017e4bfa
Blake Williams <code@shabbyrobe.org>
2020-02-23 17:22:49 +1100

    Add first pass at caps file parser

--- internal/gopher/caps.go
+++ internal/gopher/caps.go
@@ -2,29 +2,60 @@ package gopher
 
 import "time"
 
-var (
-	magic = []byte("CAPS")
-)
+type Caps interface {
+	Version() int
+	ExpiresAfter() time.Duration
 
-type Caps struct {
-	Version int
-	Expiry  time.Duration
-	Entries []Cap
+	PathConfig() (*PathConfig, error)
+	ServerInfo() (*ServerInfo, error)
+	Software() (name, version string)
+
+	// Default text encoding for content types 0 and 1.
+	// If this returns an empty string, UTF-8 is presumed.
+	DefaultEncoding() string
 }
 
-type Cap interface{}
-
-type CapKeyValue struct {
-	Key   string
-	Value string
-	Line  int
+var UnixPathConfig = PathConfig{
+	Delimiter:        "/",
+	Identity:         ".",
+	Parent:           "..",
+	ParentDouble:     false,
+	EscapeCharacter:  '\\',
+	KeepPreDelimiter: false,
 }
 
-var _ Cap = CapKeyValue{}
+type PathConfig struct {
+	// Refers to how the server separates folders from each other; Unix machines use `/`,
+	// Microsoft machines use `\`, and obsolete Macs use `:`
+	Delimiter string
 
-type CapComment struct {
-	Comment string
-	Line    int
+	// Refers to the shorthand used by an operating system to mean "this directory"; UNIX
+	// machines use `.`.
+	Identity string
+
+	// Refers to the shorthand for "the directory immediately above", and is `..` on UNIX
+	// and Microsoft systems.
+	Parent string
+
+	// Refers to an oddball feature of obsolete Macs: two consecutive path delimiters are
+	// used to refer to the parent directory. For all systems other than pre-OS X
+	// Macintoshes, this should be false.
+	ParentDouble bool
+
+	// Tells the client the escape character for quoting delimiters when they appear in
+	// selectors; most of the time, this is `\\`.
+	EscapeCharacter byte
+
+	// Tells the client not to cut everything up to the first path delimiter; most of the
+	// time, this should be `FALSE`.
+	KeepPreDelimiter bool
 }
 
-var _ Cap = CapComment{}
+type ServerInfo struct {
+	Software     string
+	Version      string
+	Architecture string
+	Description  string
+	Geolocation  string
+	AdminEmail   string
+}
---
+++ internal/gopher/capsfile_test.go
@@ -0,0 +1,46 @@
+package gopher
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/davecgh/go-spew/spew"
+)
+
+func TestParseCapsSeparateComments(t *testing.T) {
+	cf := strings.Join([]string{
+		`CAPS`,
+		`# foo`,
+		``,
+		`# bar`,
+	}, "\n")
+
+	caps, err := ParseCapsBytes("file", []byte(cf), 0)
+	if err != nil {
+		t.Fatal(err)
+	}
+	spew.Dump(caps)
+}
+
+func TestParseCapsInvalidKV(t *testing.T) {
+	for idx, tc := range []struct {
+		in string
+	}{
+		{"foo"},
+		{"$foo"},
+		{"$foo yep"},
+		{"="},
+		{"=1"},
+		{"$="},
+		{"$=1"},
+	} {
+		t.Run(fmt.Sprintf("%d", idx), func(t *testing.T) {
+			_, err := ParseCapsBytes("file", []byte("CAPS\n"+tc.in), 0)
+			if !errors.Is(err, ErrCapsKeyValueInvalid) {
+				t.Fatal()
+			}
+		})
+	}
+}
