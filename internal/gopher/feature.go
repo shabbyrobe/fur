95e76403a239bea4dfaded9a78fe7855b7bc5d01
Blake Williams <code@shabbyrobe.org>
2020-03-01 00:30:05 +1100

    Feature comments

--- internal/gopher/feature.go
+++ internal/gopher/feature.go
@@ -3,13 +3,16 @@ package gopher
 type Feature int
 
 const (
-	// ASK forms from Gopher+
+	// Server supports those weird ASK forms from Gopher+.
+	//
+	// This lib is unlikely to ever support these until evidence appears of something
+	// actually using them in the wild, which has so far not been forthcoming.
 	FeaturePlusAsk Feature = 1
 
-	// Server understands GopherII queries
+	// Server understands GopherII queries.
 	FeatureII Feature = 2
 
-	// Server will respond to GopherIIbis metadata queries
+	// Server will respond to GopherIIbis metadata queries.
 	FeatureIIbis Feature = 3
 )
 
